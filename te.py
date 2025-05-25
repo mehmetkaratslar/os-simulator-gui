9dfe8c0e700190526bb688f5fe7f2cc7668f14b5
Mehmet KARATAŞ <mehmetkarataslar@gmail.com>
2025-05-25 16:49:18 +0300

    Delete te.py

--- te.py
+++
@@ -1,8 +0,0 @@
-def get_memory_info():
-    memory = psutil.virtual_memory()
-    return {
-        'total': memory.total,
-        'available': memory.available,
-        'used': memory.used,
-        'percent': memory.percent
-    }
